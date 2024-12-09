cfb0c62e7c31cb165e14060a9ce1aa8a3546a263
RichardGreat <buyuqirol@mail.ru>
2024-12-09 08:10:28 +0500

    fix

--- lib/screen/main/main_screen.dart
+++ lib/screen/main/main_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:paloma365/bloc/history/bloc.dart';
+import 'package:paloma365/bloc/history/event.dart';
 import 'package:paloma365/screen/history/history.dart';
 import 'package:paloma365/screen/seats/seats_screen.dart';
 
@@ -27,6 +30,12 @@ class _MainScreenState extends State<MainScreen> {
           setState(() {
             _currentIndex = index;
           });
+
+          if (index == 1) {
+            // Get the HistoryBloc instance and trigger refresh
+            final historyBloc = context.read<HistoryBloc>();
+            historyBloc.add(LoadHistory());
+          }
         },
         selectedItemColor: Colors.white,
         unselectedItemColor: Colors.white70,
